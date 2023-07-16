f01badd932ea0e4d4cec46ef0b9f8bbf33b423e8
Arina Kosiakov <akaiko.social@gmail.com>
2023-07-16 23:51:32 +0200

    finished about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React, { useEffect } from 'react'
 import Image from 'next/image'
 import akai from '../../public/akai-lg.svg'
+import my_photo from '../../public/me.jpg'
 
 function AboutMe() {
     useEffect(() => {
@@ -11,25 +12,31 @@ function AboutMe() {
     return (
         <div className="page h-full flex flex-row justify-between text-white bg-gray-background">
 
-            <div className='h-full md:w-1/2 flex flex-col item-center overflow-y-scroll scrollbar-hide'>
+            <div className='h-full md:w-1/2 z-10 flex flex-col item-center overflow-y-scroll scrollbar-hide'>
 
-                <div className=' w-full p-16 font-karla text-2xl bg-gray-dark bg-opacity-80'>
+                <div className=' w-full p-10 md:p-16 font-karla text-xl md:text-2xl bg-gray-dark bg-opacity-80'>
                     <h1 className=' text-5xl font-pfd mb-10 hover:text-akai-red'> About me </h1>
 
                     <p>Hello, I&apos;m <span className=' text-akai-red'>Akai</span> .</p>
                     <hr className='h-1 my-2 opacity-0' />
                     <p><span className=' text-akai-red'>Akai</span> means red in japanese and for me Akai means being passionate, loving and energetic.</p>
                     <hr className='h-1 my-2 opacity-0' />
-                    <p>I&apos;ve been into science since I was little and currently I&apos;m studying Computer science at the University. My biggest dream is to work in the <span className=' text-akai-red'>game industry</span> and I
-                        try to improve my programming skills in order to achieve this dream.</p>
+                    <p>I&apos;ve been into technology since I was little and currently I&apos;m studying Telecomunication engineering at the University. My biggest dream is to work in the
+                        <span className=' text-akai-red'>game industry</span> and I am trying my best
+                        to improve my programming and srtistic skills in order to achieve this dream.</p>
                     <hr className='h-1 my-2 opacity-0' />
-                    <p>I have different hobbies. I consider myself an <span className=' text-akai-red'>artist</span>,
-                        <span className=' text-akai-red'> streamer</span>, <span className=' text-akai-red'> content creator</span>,
-                        <span className=' text-akai-red'> painter</span> and <span className=' text-akai-red'> web developer</span> (and yes this site, I made it! ).
-                        I am also a big fan of video games and anime. I am always opened to suggestions! </p>
+                    <p>I have different hobbies, some are being an <span className=' text-akai-red'>artist</span>,
+                        <span className=' text-akai-red'> streamer</span>,
+                        <span className=' text-akai-red'> painter</span>, and <span className=' text-akai-red'> web developer</span> (and yes this site, I made it! ).
+                        I like to work out and in my free time I spend my time playing video games and watching animes. I am always opened to suggestions! </p>
                     <hr className='h-1 my-2 opacity-0' />
-                    <p> <Link href={'https://github.com/akaik0'} > <span className=' text-akai-red'>Here</span></Link> you can find some of my technical projects. </p>
                 </div>
+
+                <div className='w-full flex flex-col  items-center my-3 bg-gray-dark bg-opacity-80'>
+                    <div><p className='py-3 text-sm md:text-lg lg:text-xl'>Little spoiler, this is me. <span className=' text-akai-red'>Hi!</span></p></div>
+                    <Image src={my_photo} alt='logo' className='w-3/4 md:w-2/4 rounded-md mb-12'></Image>
+                </div>
+
             </div>
             <div className='h-full z-0 md:w-1/2  relative'>
                 <Image src={akai} alt='logo' className=' absolute bottom-8 right-14 scale-125'></Image>
