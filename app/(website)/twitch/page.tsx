fb64e7bd6e5481ab3d1bbe2cfe2c710f151a4823
Arina Kosiakov <akaiko.social@gmail.com>
2024-01-04 16:52:26 +0100

    changes

--- app/(website)/twitch/page.tsx
+++ app/(website)/twitch/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
 import Image from "next/image";
-import akai from "../../public/akai-lg.svg";
+import akai from "../../../public/akai-lg.svg";
 import Link from "next/link";
 
 function Twitch() {
@@ -10,10 +10,10 @@ function Twitch() {
   });
 
   return (
-    <div className="page h-full flex flex-row justify-between text-white bg-gray-background">
-      <div className="h-full z-10 w-1/2 flex flex-col item-center overflow-y-scroll scrollbar-hide">
-        <div className=" w-full h-full p-16 font-karla text-2xl bg-gray-dark bg-opacity-80">
-          <h1 className=" text-5xl font-pfd mb-10 hover:text-akai-red">
+    <div className="page flex h-full flex-row justify-between bg-gray-background text-white">
+      <div className="item-center z-10 flex h-full w-1/2 flex-col overflow-y-scroll scrollbar-hide">
+        <div className=" h-full w-full bg-gray-dark bg-opacity-80 p-16 font-karla text-2xl">
+          <h1 className=" font-pfd mb-10 text-5xl hover:text-akai-red">
             {" "}
             <Link href={"https://www.twitch.tv/ak_aiko"}>Twitch</Link>{" "}
           </h1>
@@ -27,10 +27,10 @@ function Twitch() {
             like to share also how I create my{" "}
             <span className=" text-akai-red">art</span>.
           </p>
-          <hr className="h-1 my-2 opacity-0" />
+          <hr className="my-2 h-1 opacity-0" />
         </div>
 
-        <div className=" w-full h-full mt-5 p-16 font-karla text-2xl bg-gray-dark bg-opacity-80">
+        <div className=" mt-5 h-full w-full bg-gray-dark bg-opacity-80 p-16 font-karla text-2xl">
           <p>
             {" "}
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
@@ -41,7 +41,7 @@ function Twitch() {
         </div>
       </div>
 
-      <div className="h-full z-0 w-1/2  relative">
+      <div className="relative z-0 h-full  w-1/2">
         <Image
           alt="logo"
           src={akai}
