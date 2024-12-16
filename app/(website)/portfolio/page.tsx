ae2919f52b1f76a05f367a2535f77aa58fe6081d
Arina Kosiakov <arina.kosiakov@zepping.it>
2024-12-16 18:01:05 +0100

    portfolio

--- app/(website)/portfolio/page.tsx
+++ app/(website)/portfolio/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import CardCarousel from "@/app/components/carousel";
 
 import Sketch5 from "../../../public/sketches/01.jpg";
 import Sketch4 from "../../../public/sketches/04.jpg";
@@ -8,8 +9,23 @@ import Sketch2 from "../../../public/concepts/kiri_expressions.jpg";
 
 function Concepts() {
   return (
-    <div className="grid h-full grid-cols-1 grid-rows-5 place-items-stretch gap-3">
-      WIP
+    <div className="flex h-full flex-col justify-center">
+      <div className="mb-24">
+        <div className="mb-6 font-eiko text-2xl">Projects</div>
+        <CardCarousel type="projects" cards={} />
+      </div>
+      <div className="mb-24">
+        <div className="mb-6 font-eiko text-2xl">Environments</div>
+        <CardCarousel type="environments" />
+      </div>
+      <div className="mb-24">
+        <div className="mb-6 font-eiko text-2xl">Sketchbook</div>
+        <CardCarousel type="sketchbook" />
+      </div>
+      <div className="mb-24">
+        <div className="mb-6 font-eiko text-2xl">Illustrations</div>
+        <CardCarousel type="illustrations" />
+      </div>
     </div>
   );
 }
---
+++ app/components/carousel.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import React, { FC, RefObject, useEffect, useState } from "react";
+import { Card } from "flowbite-react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import Image, { StaticImageData } from "next/image";
+import bg from "../../public/homepage_imgs/env.jpg";
+import Link from "next/link";
+
+type CardData = {
+  title: string;
+  description: string;
+  src: StaticImageData;
+  id: string;
+};
+
+type CardCarouselProps = {
+  type: string;
+  cards?: CardData[];
+};
+
+const CardCarousel: FC<CardCarouselProps> = ({ type }) => {
+  const carouselRef: RefObject<HTMLDivElement> = React.useRef(null);
+
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+
+  const cards: CardData[] = [
+    {
+      title: "Card 1",
+      description: "Description for card 1",
+      src: bg,
+      id: "",
+    },
+    {
+      title: "Card 2",
+      description: "Description for card 2",
+      src: bg,
+      id: "1",
+    },
+    {
+      title: "Card 3",
+      description: "Description for card 3",
+      src: bg,
+      id: "",
+    },
+    {
+      title: "Card 4",
+      description: "Description for card 4",
+      src: bg,
+      id: "",
+    },
+    {
+      title: "Card 5",
+      description: "Description for card 4",
+      src: bg,
+      id: "",
+    },
+    {
+      title: "Card 6",
+      description: "Description for card 4",
+      src: bg,
+      id: "",
+    },
+  ];
+  const scrollLeft = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
+
+  const handleScroll = () => {
+    if (carouselRef.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = carouselRef.current;
+      setCanScrollLeft(scrollLeft > 0);
+      setCanScrollRight(scrollLeft < scrollWidth - clientWidth);
+    }
+  };
+  useEffect(() => {
+    if (carouselRef.current) {
+      handleScroll();
+      carouselRef.current.addEventListener("scroll", handleScroll);
+      return () => {
+        carouselRef.current?.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+
+  return (
+    <div className="relative flex w-full justify-center">
+      {/* Left Navigation Button */}
+      <button
+        className={`absolute left-0 z-10 flex h-full w-[3%] items-center justify-center bg-custom_gray-700 bg-opacity-30 p-2 backdrop-blur-sm ${!canScrollLeft ? "opacity-0" : ""}`}
+        onClick={scrollLeft}
+        disabled={!canScrollLeft}
+      >
+        <FaChevronLeft />
+      </button>
+      {/* Card Container */}
+      <div
+        ref={carouselRef}
+        className="no-scrollbar flex w-full gap-4 overflow-x-auto scroll-smooth px-12"
+        style={{ scrollBehavior: "smooth" }}
+      >
+        {cards.map((card, index) => (
+          <div key={index} className="min-w-[300px]">
+            <Card
+              className="border-none"
+              renderImage={() => (
+                <Image width={500} height={250} src={card.src} alt="image 1" />
+              )}
+              href={`/portfolio/${type}/${card.id}`}
+              // href={`/portfolio/illustrations`}
+            >
+              {/* <Image src={card.src} alt="image" width={500} height={2500} /> */}
+              <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {card.title}
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                {card.description}
+              </p>
+            </Card>
+          </div>
+        ))}
+      </div>
+      {/* Right Navigation Button */}
+      <button
+        className={`absolute right-0 z-10 flex h-full w-[3%] items-center justify-center bg-custom_gray-700 bg-opacity-30 p-2 backdrop-blur-sm ${!canScrollRight ? "opacity-0" : ""}`}
+        onClick={scrollRight}
+        disabled={!canScrollRight}
+      >
+        <FaChevronRight />
+      </button>
+    </div>
+  );
+};
+
+export default CardCarousel;
+
+// Usage Example:
+// import CardCarousel from './CardCarousel';
+// const cards: CardData[] = [
+//   { title: 'Card 1', description: 'Description for card 1' },
+//   { title: 'Card 2', description: 'Description for card 2' },
+//   { title: 'Card 3', description: 'Description for card 3' },
+//   { title: 'Card 4', description: 'Description for card 4' },
+// ];
+// <CardCarousel cards={cards} />;
