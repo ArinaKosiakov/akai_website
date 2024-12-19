b4570ba019297316e3e111154449bad6a65a9f4c
Arina Kosiakov <arina.kosiakov@zepping.it>
2024-12-19 17:13:33 +0100

    fix

--- app/components/dotNavigator.tsx
+++ app/components/dotNavigator.tsx
@@ -8,28 +8,34 @@ type DotNavigatorProp = {
 
 function DotNavigator({ src_arr, onNavigate }: DotNavigatorProp) {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [activeEvent, setActiveEvent] = useState<number>(0);
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [canMoveLeft, setCanMoveLeft] = useState(true);
+  const [canMoveRight, setCanMoveRight] = useState(true);
 
   const handleNavigate = (index: number) => {
-    if (currentIndex === 0 || currentIndex === src_arr.length - 1) {
-      setIsDisabled(true);
+    if (index === 0) {
+      setCanMoveLeft(false);
+    }
+    if (index === src_arr.length - 1) {
+      setCanMoveRight(false);
     }
     if (index >= 0 && index < src_arr.length) {
+      if (index > 0) {
+        setCanMoveLeft(true);
+      }
+
       setCurrentIndex(index);
+      console.log(currentIndex);
+
       if (onNavigate) {
         onNavigate(index);
       }
     }
-    console.log(currentIndex);
-    console.log(isDisabled);
-    setActiveEvent(index);
   };
 
   return (
     <div className="flex w-full flex-row items-center justify-between md:w-[70%] lg:w-[40%] xl:w-[30%]">
       <button
-        disabled={isDisabled}
+        disabled={!canMoveLeft}
         onClick={() => handleNavigate(currentIndex - 1)}
       >
         <FaChevronLeft />
@@ -41,14 +47,14 @@ function DotNavigator({ src_arr, onNavigate }: DotNavigatorProp) {
           onClick={() => handleNavigate(index)}
           className="h-3 w-3 rounded-full border border-white bg-custom_gray-700 hover:cursor-pointer hover:bg-akai-500"
           style={{
-            backgroundColor: index === activeEvent ? "#CB4949" : "",
-            border: index === activeEvent ? "#CB4949" : "",
+            backgroundColor: index === currentIndex ? "#CB4949" : "",
+            border: index === currentIndex ? "#CB4949" : "",
           }}
         ></div>
       ))}
 
       <button
-        disabled={isDisabled}
+        disabled={!canMoveRight}
         onClick={() => handleNavigate(currentIndex + 1)}
       >
         <FaChevronRight />
