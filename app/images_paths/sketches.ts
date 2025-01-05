56a6d1e26318f2ec047f01dca66e12ea7db3da5f
Arina Kosiakov <akaiko.social@gmail.com>
2025-01-06 00:20:44 +0100

    fix

--- app/images_paths/sketches.ts
+++ app/images_paths/sketches.ts
@@ -16,107 +16,104 @@ import s17 from "../../public/portfolio_imgs/sketches/17.jpg";
 import s18 from "../../public/portfolio_imgs/sketches/18.jpg";
 import s19 from "../../public/portfolio_imgs/sketches/19.jpg";
 
-function sketches() {
-  const sketches = [
-    {
-      title: "Hands",
-      url: s1,
-      description: "Practice of hand anatomy.",
-      date: "September 2023",
-    },
-    {
-      title: "Hands and Feet",
-      url: s4,
-      description: "Practise of hand and feet anatomy.",
-      date: "September 2023",
-    },
+const sketches = [
+  {
+    title: "Hands",
+    url: s1,
+    description: "Practice of hand anatomy.",
+    date: "September 2023",
+  },
+  {
+    title: "Hands and Feet",
+    url: s4,
+    description: "Practise of hand and feet anatomy.",
+    date: "September 2023",
+  },
 
-    {
-      title: "Sketch",
-      url: s17,
-      description: "Quick sketch practice (5 mins)",
-      date: "October 2023",
-    },
-    {
-      title: "Sketch",
-      url: s18,
-      description: "Quick sketch practice (5 mins)",
-      date: "October 2023",
-    },
-    {
-      title: "Anatomy",
-      url: s16,
-      description: "Practice of body anatomy.",
-      date: "October 2023",
-    },
-    {
-      title: "Portraits",
-      url: s5,
-      description: "Practice of planes of the face",
-      date: "January 2024",
-    },
-    {
-      title: "Portraits",
-      url: s6,
-      description: "Practice of planes of the face",
-      date: "January 2024",
-    },
-    {
-      title: "Portraits",
-      url: s7,
-      description: "Practice of planes of the face",
-      date: "January 2024",
-    },
-    {
-      title: "Anatomy",
-      url: s8,
-      description: "Gesture drawing  practice (5 mins)",
-      date: "January 2024",
-    },
-    {
-      title: "Portraits",
-      url: s9,
-      description: "Practice of planes of the face",
-      date: "January 2024",
-    },
-    {
-      title: "Portraits",
-      url: s10,
-      description: "Practice of planes of the face",
-      date: "January 2024",
-    },
-    {
-      title: "Portraits",
-      url: s11,
-      description: "Practice of planes of the face",
-      date: "January 2024",
-    },
-    {
-      title: "Cats and dogs",
-      url: s12,
-      description: "Animal anatomy studies.",
-      date: "March 2024",
-    },
-    {
-      title: "Rats",
-      url: s13,
-      description: "Animal anatomy studies.",
-      date: "March 2024",
-    },
-    {
-      title: "Turtles",
-      url: s14,
-      description: "Animal anatomy studies.",
-      date: "March 2024",
-    },
-    {
-      title: "Penguins",
-      url: s15,
-      description: "Animal anatomy studies.",
-      date: "March 2024",
-    },
-  ];
-  return sketches;
-}
+  {
+    title: "Sketch",
+    url: s17,
+    description: "Quick sketch practice (5 mins)",
+    date: "October 2023",
+  },
+  {
+    title: "Sketch",
+    url: s18,
+    description: "Quick sketch practice (5 mins)",
+    date: "October 2023",
+  },
+  {
+    title: "Anatomy",
+    url: s16,
+    description: "Practice of body anatomy.",
+    date: "October 2023",
+  },
+  {
+    title: "Portraits",
+    url: s5,
+    description: "Practice of planes of the face",
+    date: "January 2024",
+  },
+  {
+    title: "Portraits",
+    url: s6,
+    description: "Practice of planes of the face",
+    date: "January 2024",
+  },
+  {
+    title: "Portraits",
+    url: s7,
+    description: "Practice of planes of the face",
+    date: "January 2024",
+  },
+  {
+    title: "Anatomy",
+    url: s8,
+    description: "Gesture drawing  practice (5 mins)",
+    date: "January 2024",
+  },
+  {
+    title: "Portraits",
+    url: s9,
+    description: "Practice of planes of the face",
+    date: "January 2024",
+  },
+  {
+    title: "Portraits",
+    url: s10,
+    description: "Practice of planes of the face",
+    date: "January 2024",
+  },
+  {
+    title: "Portraits",
+    url: s11,
+    description: "Practice of planes of the face",
+    date: "January 2024",
+  },
+  {
+    title: "Cats and dogs",
+    url: s12,
+    description: "Animal anatomy studies.",
+    date: "March 2024",
+  },
+  {
+    title: "Rats",
+    url: s13,
+    description: "Animal anatomy studies.",
+    date: "March 2024",
+  },
+  {
+    title: "Turtles",
+    url: s14,
+    description: "Animal anatomy studies.",
+    date: "March 2024",
+  },
+  {
+    title: "Penguins",
+    url: s15,
+    description: "Animal anatomy studies.",
+    date: "March 2024",
+  },
+];
 
 export default sketches;
